94a8352788366727acd59471d1b897b7096fa68d
djrich87 <djrich87@gmail.com>
2022-02-05 14:16:24 -0700

    TTT lab

--- js/app.js
+++ js/app.js
@@ -4,74 +4,65 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 let squares, winner, turn
-
-// const player0 = -1
-// const playerX = 1
-// let winner = null
-// let tie = 'T'
+// let playerTurn = [o,x]
+// let gameOutcome = [tie, winner, loser]
+const player0 = -1
+const playerX = 1
+//let winner = null
 
 /*------------------------ Cached Element References ------------------------*/
 const boardSquares = document.querySelector(".board")
-// console.log(boardSquares);
-// need a way to update board squares in cached reference
-// use squares an array to update board sqaures accordingly
+console.log(boardSquares);
+//Need a way to update board squares in the cached references. IN your render function, use squares - an array, to update board squares accordingly.
 
 /*----------------------------- Event Listeners -----------------------------*/
 boardSquares.addEventListener("click", handleClick);
-  // console.log()
 
 
 /*-------------------------------- Functions --------------------------------*/
-
 function  handleClick(event) {
-  // console.log(event.target.id.replace('sq',''));
+  //console.log(event.target.id);
   //change the state of the squares in the handleClick function
   //string.slice- helps remove SQ
   //
-  const id = event.target.id.replace('sq','') //
-  squares[id]= 1
+  const id = event.target.id.replace('sq','')
+  squares[id] = 1
   console.log(squares)
+  if(squares === 1){
+    return X
+  }
+  render()
 }
-
-/*----------------------------------------------------------------------------*/
 
 init();
-    // X 3.1
 
-function init() {
+function init(){
 
-  squares = [
-    null, null, null,
-    null, null, null,
-    null, null, null];
-console.log(squares)
-  // X 3.2.1
-  // 9 elements "map" to each squarfe: [0] = top-left sqaure
-  // 1] = top-middle square... [8] = bottom right square
-  // turn = {
-  //   '1': {
-  //     name: 'X'
-  //   }, '2': {
-  //     name: '0'
-  //   }
-  // }
-// 3.2.2
+squares = [
+  null, null, null,
+  null, null, null,
+  null, null, null]
+console.log(squares)  //these nulls refer to the game board in order
 
+  
 winner = null;
-// 3.2.3
 
-render ()
+render()
 
 }
-//3.3.3
 
-function render () {
-  for (let i = 0; i < squares.length; i++) {
-    console.log(squares[i])
-    // boardSquares = document.querySelector.replace APPLY THIS TO THE PROCESS TO HELP WITH THE SQUARE ARRAY
-    // adjust 
-  }
- 
+function render() {
 
-
+  for (let i = 0; i < squares.length; i++){
+  // console.log(squares[i])
+  if (squares[i] === 1){
+    boardSquares.children[i].textContent = 'x'
+  } else {
+    (squares[i] != 1)
+    boardSquares.children[i].textContent = '0'
+  } 
+  //else (boardSquares.children[i].textContent = '' )
+    //if else statement could be good here
+    //if the condition is true, then print a value to the board
+}
 }
