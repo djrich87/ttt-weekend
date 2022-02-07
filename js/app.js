ed12dd76ccfff05f589bb4bf5742b61550b8755d
djrich87 <djrich87@gmail.com>
2022-02-06 20:42:08 -0700

    ttt lab

--- js/app.js
+++ js/app.js
@@ -16,18 +16,21 @@ let squares, winner, turn
 // let gameOutcome = [isTie, winner, loser]
 const player0 = -1
 const playerX = 1
+let numOfTurns
 //const isTie = // === no winning combos found on the board, != winningCombos
 
 
 /*------------------------ Cached Element References ------------------------*/
 const boardSquares = document.querySelector(".board")
 console.log(boardSquares);
+
 const message = document.getElementById("message")
-const resetBtn = document.getElementById('resetBtn')
 
 // const winnerLoser = document.querySelector("#winnerloser")
 // console.log(winnerloser)
 //Need a way to update board squares in the cached references. IN your render function, use squares - an array, to update board squares accordingly.
+
+const resetBtn = document.getElementById('resetBtn')
 
 /*----------------------------- Event Listeners -----------------------------*/
 boardSquares.addEventListener("click", handleClick);
@@ -39,10 +42,12 @@ function  handleClick(event) {
   //change the state of the squares in the handleClick function
   //string.slice- helps remove SQ
   //handle click function is running 
+  //
   const id = event.target.id.replace('sq','')
   if (squares[id] === null){
     squares[id] = turn
     turn *= -1
+    numOfTurns += 1
     console.log(squares)
     render()
     getWinner()
@@ -58,8 +63,12 @@ squares = [
   null, null, null,
   null, null, null]
 console.log(squares)  //these nulls refer to the game board in order
+
 turn = 1
 winner = null;
+numOfTurns = 0
+
+message.innerText = ''
 render()
 }
 
@@ -70,40 +79,59 @@ function render() {
   // console.log(squares[i])
     if (squares[i] === 1){
     boardSquares.children[i].textContent = 'X'
+    message.textContent = 'Turn: 0'
     } else if 
     (squares[i] === -1) {
     boardSquares.children[i].textContent = '0'
+    message.textContent = 'Turn: X'
     } else {
     boardSquares.children[i].textContent = ""
     }
   }
+  
+
+  
 }
+
+// function whoseTurn(){
+//   turn *= -1
+//   if (turn === 1){
+//     message.textContent = 'Turn: X'
+//   }
+//   else if (turn === -1){
+//     message.textContent = 'Turn: O'
+//   }
+// }
 
 resetBtn.addEventListener('click', init)
-  render()
+
+  // render()
 
 
-//we are trying to represent a winner between 1, -1
-//there can also be a tie
-//use a for statement to set up the getWinner function
 function getWinner(){
-  for (let i = 0; i < winningCombos.length; i++){
-  // const winCondition = winningCombos[i];
-  // console.log(`This is instance ${i} `)
-        const a = winningCombos[i][0]
-        const b = winningCombos[i][1]
-        const c = winningCombos[i][2]
-        // console.log(a,b, c)
-      if (squares[a] + squares[b] + squares[c] === 3){
-          console.log('X wins')
-          message.textContent = 'X wins';
-       } else if (squares[a] + squares[b] + squares[c] === -3){
-          console.log('0 wins')
-          message.textContent = '0 wins';
-       }
+  for(let i = 0; i < winningCombos.length; i++){
+  //const winner = winningCombos[i]; 
+  // console.log(`This is instance ${i}`)
+ const a = winningCombos[i][0]
+ const b = winningCombos[i][1]
+ const c = winningCombos[i][2]
+//  console.log(a, b, c)
+//   console.log(`This is the third value`, winningCombos[i][2])
+    if (squares[a] + squares[b] + squares[c] === 3){
+      console.log('X wins')
+       message.textContent = 'X wins';
+    } else if (squares[a] + squares[b] + squares[c] === -3){
+      console.log('O wins')
+      message.textContent = 'O wins';
+    }
+    
+      
+  }  
+  if(numOfTurns === 9 && winner === null){
+    console.log('Tie')
+    message.textContent = 'Tie';
   }
-  console.log()
+  
 }
 init()
-
-getWinner()
+// getWinner()
